f5e396d9d403b52134c2d2b14cdc558c42268620
orlovstepan <orlovst94@gmail.com>
2023-03-01 15:48:42 +0100

    all tests working, fetching the data properly

--- src/Components/Startup/StartupList.tsx
+++ src/Components/Startup/StartupList.tsx
@@ -1,24 +1,40 @@
 import { display } from "@mui/system";
 import { Fragment, ReactElement, useState, useEffect } from "react";
 import { List, ListItem, ListSubheader, ListItemText, CardHeader, Box } from '@mui/material'
+import { StartupHttpService } from "../../Http/Startup/Startup.http.service";
+import { Startup } from "../../Types/Startup";
 
 export default function StartupList(): ReactElement {
-  const [startUps, setStartups] = useState([]);
+  const [startUps, setStartups] = useState<Startup[]>([]);
 
   console.log(startUps);
 
+  const styles={
+    overview: {
+      display:"flex", 
+      color:"grey",
+      fontSize:"0.8rem",
+    },
+    "overview--item": {
+      padding: "0px",
+      marginTop: "-10px",
+      backgroundColor: "transparent",
+    }
+    
+  }
+
   useEffect(() => {
-    fetch("/api/startups")
-  .then((response) => response.json())
-  .then(data => setStartups(data));    
-  },[])
+    StartupHttpService.getStartups().then(response => setStartups(response))
+    },[])
+    
 
   return <Fragment> 
-      <List>
+      <List id="startup-list" >
         {startUps.map((startUp : any) => 
-        <ListItem alignItems="flex-start" sx={{
+        <ListItem className="MuiGrid-item" sx={{
           display:"flex",
           flexDirection:"column",
+          alignItems:"flex-start",
           justifyContent:'center',
           backgroundColor:"white",
           width:"90vw",
@@ -26,8 +42,11 @@ export default function StartupList(): ReactElement {
           }}
           key={startUp.id}> 
           <CardHeader sx={{padding:"10px 0px 0px 0px"}} title={startUp.name}></CardHeader>  
-          <Box sx={{display:"flex", color:"grey"}}>
-            <ListSubheader sx={{fontSize:"0.8rem", padding:"0px", marginTop:"-10px", backgroundColor:"transparent"}}> Founded: {new Date(startUp.dateFounded).getFullYear()} | {startUp.employees} employees | {startUp.totalFunding} $ | {startUp.currentInvestmentStage} |  </ListSubheader>
+          <Box className="overview" sx={styles.overview}>
+            <ListSubheader sx={styles["overview--item"]}> Founded: {new Date(startUp.dateFounded).getFullYear()} |</ListSubheader>
+            <ListSubheader sx={styles["overview--item"]}> {startUp.employees} employees |</ListSubheader>
+            <ListSubheader sx={styles["overview--item"]}> {startUp.totalFunding} $ |</ListSubheader>
+            <ListSubheader sx={styles["overview--item"]}> {startUp.currentInvestmentStage} </ListSubheader>
           </Box>
           <ListItemText className="description"> {startUp.shortDescription}</ListItemText>
         </ListItem>)}
